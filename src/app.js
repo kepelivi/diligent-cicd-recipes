056cca5976a1c1d34eb28edab4229a6ee32e6c7e
kepelivi <kepelivi7@gmail.com>
2024-10-02 14:17:31 +0200

    finish tasks

--- src/app.js
+++ src/app.js
@@ -27,22 +27,48 @@ function createContainer({ onShow }) {
   const container = element('div', { class: 'container' }, [
     element('h1', {}, ['My Recipes']),
     element('button', { class: 'btn btn-primary', onClick: onShow }, ['Show Recipes']),
-    element('div', { id: 'recipeList' }),
+    element('div', { id: 'recipeList', class: "container my-5" }),
   ])
   return container;
 }
 
 export function setupApp(root) {
-  
+
   let isVisible = false;
-  
+
   function handleShow(event) {
     isVisible = !isVisible;
     const list = event.target.parentNode.querySelector('#recipeList');
-    
-    
-    if (isVisible) {      
-      list?.appendChild(element('p', {}, ['Recipe List']));
+
+    const recipes = getRecipes();
+    console.log(recipes)
+
+    if (isVisible) {
+      list?.appendChild(element("div", { class: "row g-4" }));
+      const row = event.target.parentNode.querySelector(".row");
+      recipes.forEach(recipe => {
+        const card = document.createElement("div");
+        card.className = "col-12 col-sm-6 col-md-4 col-lg-3 mb-4";
+
+        card.innerHTML = `
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${recipe.name}</h5>
+              <p class="card-text"><strong>Preparation Time:</strong> ${recipe.preparation_time}</p>
+              <p class="card-text"><strong>Servings:</strong> ${recipe.servings}</p>
+              <p class="card-text"><strong>Ingredients:</strong></p>
+              <ul class="list-unstyled">
+                ${recipe.ingredients.map(ingredient => `<li>${ingredient.item} - ${ingredient.quantity}</li>`).join("")}
+              </ul>
+              <p><strong>Instructions:</strong></p>
+              <ol>
+                ${recipe.instructions.map(instruction => `<li>${instruction}</li>`).join("")}
+              </ol>
+            </div>
+          </div>
+        `;
+        row.appendChild(card);
+      })
     } else {
       list.innerText = '';
     }
